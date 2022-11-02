4dc335d59f128e57b4588e1cc0d1ff53396b5b2b
VladNedr <nedr2517@mail.ru>
2022-11-02 21:25:40 +0300

    add: mobile menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
+const mMenuToggle = document.querySelector(".mobile-menu-toggle");
+const menu = document.querySelector(".mobile-menu");
+//  Последние два пункта кода: При клике на mobile-menu(hamburger) появляется окно.
+
+
 window.addEventListener("scroll", () => {
   if (this.scrollY > 1) {
     navbar.classList.add("navbar-light");
@@ -10,3 +15,9 @@ window.addEventListener("scroll", () => {
   }
 });
 //  JS - fixed шапка сайта , меняет свой цвет по мере scroll по странице
+
+mMenuToggle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.toggle('is-open');
+});
+//  Тоже относится к hamburger, при клике выводит мобильное меню!
