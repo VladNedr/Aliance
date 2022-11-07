a7c06e206a6e0dfe639058340c5609345da3cb5b
VladNedr <nedr2517@mail.ru>
2022-11-07 11:33:35 +0300

    fix: header-features for overflow

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ mMenuToggle.addEventListener("click", (event) => {
 
 const swiper = new Swiper(".swiper", {
   speed: 400,
-  autoHeight: true,
+  // autoHeight: true,
   // loop: true,
   slidesPerView: 1,
   navigation: {
